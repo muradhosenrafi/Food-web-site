35d8dee538b870e57b78eb148b933563dce4b7a5
muradhosenrafi <muradhosenrafi@gmail.com>
2026-07-22 00:04:05 +0600

    middle column add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,59 +1,90 @@
-import React, { useState } from 'react'
-import { BiChevronRight } from 'react-icons/bi';
-import { FaRegEnvelope } from 'react-icons/fa'
+import React, { useState } from "react";
+import { BiChevronRight } from "react-icons/bi";
+import { FaRegEnvelope } from "react-icons/fa";
+
+const navItems = [
+  { name: "Home", link: "/" },
+  { name: "Menu", link: "/menu" },
+  { name: "About Us", link: "/about" },
+  { name: "Contact", link: "/contact" },
+];
 
 const Footer = () => {
-
-  const [email,setEmail] =useState ('');
-  const handleSubmit = (e)=> {
-   e.preventDefault();
-   alert ( `Thanks for subscribing! We'll send updates to ${email}`) ;
-   setEmail('');
-  }
+  const [email, setEmail] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Thanks for subscribing! We'll send updates to ${email}`);
+    setEmail("");
+  };
 
   return (
-    <footer className='bg-[#2A211C] text-amber-100 py-12 px-4 sm:px-6 lg:px-8 relative overflow-hidden '>
-      <div className='max-w-7xl mx-auto relative z-10'>
-         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mb-12'>
-        {/* left colum */}
-        <div className='space-y-6'>
+    <footer className="bg-[#2A211C] text-amber-100 py-12 px-4 sm:px-6 lg:px-8 relative overflow-hidden ">
+      <div className="max-w-7xl mx-auto relative z-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mb-12">
+          {/* left colum */}
+          <div className="space-y-6">
+            <h2 className="text-4xl sm:text-5xl md:text-5xl font-bold font-dim  text-amber-400 animate-pulse">
+              Foodie-Frenzy
+            </h2>
+            <p className="text-amber-200/90 text-sm font-dim italic">
+              When culinary artistry meets doorstep convenience . <br />
+              Savor handcrafted perfection delivered with care .
+            </p>
+            <form onSubmit={handleSubmit} className="relative mt-4 group ">
+              <div className="flex items-center gap-2 mb-2">
+                <FaRegEnvelope className="text-amber-400 animate-pulse" />
+                <span className="font-bold text-amber-400">
+                  Get Exclusive Offers
+                </span>
+              </div>
 
-        <h2 className='text-4xl sm:text-5xl md:text-5xl font-bold font-dim  text-amber-400 animate-pulse'>
-           Foodie-Frenzy
-        </h2>
-        <p className='text-amber-200/90 text-sm font-dim italic'>
-         When culinary artistry meets doorstep convenience . <br />
-         Savor handcrafted perfection delivered with care .
-        </p>
-        <form onSubmit={handleSubmit} className='relative mt-4 group '>
-          <div className='flex items-center gap-2 mb-2'>
-            <FaRegEnvelope className='text-amber-400 animate-pulse'/>
-              <span className='font-bold text-amber-400'>Get Exclusive Offers</span>
+              <div className="relative">
+                <input
+                  type="email"
+                  placeholder="Enter your email..."
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full px-4 py-2.5 rounded-lg bg-amber-50/5 border-amber-400/30 focus:border-amber-400 pr-24 placeholder-amber-200/50 transition-all duration-300 focus:ring-amber-400/20  focus:ring-4  focus:outline-none"
+                  required
+                />
+                <button
+                  type="submit"
+                  className="absolute right-1 top-1 bg-gradient-to-br from-amber-300 via-orange-500 to-amber-600 text-white px-4 py-2 rounded-full flex items-center gap-1.5 shadow-lg hover:shadow-amber-400/30 overflow-hidden transition-all duration-500"
+                >
+                  <span className="font-bold text-sm tracking-wide transition-transform duration-300 group-hover:-translate-x-1">
+                    Join Now
+                  </span>
+                  <BiChevronRight className="text-xl transition-transform duration-300 group-hover:animate-spin flex-shrink-0" />
+                  <span className="absolute inset-0 -translate-x-full bg-gradient-to-r from-transparent via-amber-50/30 to-transparent group-hover:translate-x-full transition-transform duration-700 " />
+                </button>
+              </div>
+            </form>
           </div>
-          
-          <div className='relative'>
-            <input type="email" placeholder='Enter your email...' value={email}
-            onChange={e => setEmail(e.target.value)}
-            className='w-full px-4 py-2.5 rounded-lg bg-amber-50/5 border-amber-400/30 focus:border-amber-400 pr-24 placeholder-amber-200/50 transition-all duration-300 focus:ring-amber-400/20  focus:ring-4  focus:outline-none'
-           required />
-           <button type='submit' className='absolute right-1 top-1 bg-gradient-to-br from-amber-300 via-orange-500 to-amber-600 text-white px-4 py-2 rounded-full flex items-center gap-1.5 shadow-lg hover:shadow-amber-400/30 overflow-hidden transition-all duration-500'>
-             <span className='font-bold text-sm tracking-wide transition-transform duration-300 group-hover:-translate-x-1'>
-                 Join Now
-             </span>
-             <BiChevronRight className='text-xl transition-transform duration-300 group-hover:animate-spin flex-shrink-0'/>
-             <span className='absolute inset-0 -translate-x-full bg-gradient-to-r from-transparent via-amber-50/30 to-transparent group-hover:translate-x-full transition-transform duration-700 '/>
-
-           </button>
+          {/* middle column */}
+          <div className="flex justify-center">
+            <div className="space-y-4">
+              <h3 className="text-xl font-semibold mb-4 border-l-4 border-l-amber-400 font-merriweather italic text-amber-300 pl-2">
+                Navigation
+              </h3>
+              <ul className="space-y-3">
+                {navItems.map((item) => (
+                  <li key={item.name}>
+                    <a
+                      href={item.link}
+                      className="flex items-center hover:text-amber-400 transition-all group font-merriweather hover:pl-2"
+                    >
+                      <BiChevronRight className="mr-2 text-amber-400 group-hover:animate-bounce" />
+                      <span className="hover:italic">{item.name}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </form>
         </div>
-         {/* middle column */}
-         
-       </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
-
+export default Footer;
