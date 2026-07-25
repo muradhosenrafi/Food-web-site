6566b6f4496cecb62a52219bea1d187648f35b04
muradhosenrafi <muradhosenrafi@gmail.com>
2026-07-26 00:45:51 +0600

    about section add

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
+import OurMenu from '../components/OurMenu'
 
 const Menu = () => {
   return (
    <div>
      <NavBar/>
+     <OurMenu/>
      <Footer/>
    </div>
   )
